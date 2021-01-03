8d786584af2f0a2a8dcba8e43f3b2ec6061a8da6
yukiinoue <yuki.inoue@desamis.co.jp>
2021-01-03 17:54:07 +0900

    get_candles() のバグを修正

--- oanda/oanda.py
+++ oanda/oanda.py
@@ -15,34 +15,29 @@ class Client:
         self.__api = API(api_key, self.__env)
         self.__account_id = account_id
 
-    def get_candles_s5(self, instrument: str, date_from: datetime, date_to: datetime):
+    def get_candles(self, instrument: str, date_from: datetime, date_to: datetime, granularity: str):
         timestamp_from = math.floor(date_from.timestamp())
         timestamp_to = math.floor(date_to.timestamp())
-        print(timestamp_from)
-        print(timestamp_to)
-        print("hogeaaaaaaaaa")
+        granularity_s = self.__get_granularity_s(granularity)
         candles = []
         while timestamp_to - timestamp_from > 0:
-            print("destance" + str(timestamp_to - timestamp_from))
-            print("hoge" + str(timestamp_from))
             _from = timestamp_from
-            _to = timestamp_from + 5000 * 5
+            _to = timestamp_from + 5000 * granularity_s
             if _to > timestamp_to:
                 _to = timestamp_to
             params = {
                 "from": datetime.fromtimestamp(_from).isoformat(),
                 "to": datetime.fromtimestamp(_to).isoformat(),
-                "granularity": "S5"
+                "granularity": granularity
             }
             r = instruments.InstrumentsCandles(instrument, params)
             resp = self.__api.request(r)
-            candles.append(resp["candles"])
+            candles += resp["candles"]
             timestamp_from = _to
         data = []
         for candle in candles:
             data.append([
-                candle["time"],
-                # datetime.fromisoformat(candle["time"][:19]),
+                datetime.fromisoformat(candle["time"].split(".")[0]),
                 candle["mid"]["o"],
                 candle["mid"]["h"],
                 candle["mid"]["l"],
@@ -51,6 +46,45 @@ class Client:
             ])
         return data
 
-
-def foge():
-    print("hoge")
+    @staticmethod
+    def __get_granularity_s(granularity: str):
+        if granularity == "S5":
+            return 5
+        if granularity == "S10":
+            return 10
+        if granularity == "S15":
+            return 15
+        if granularity == "S30":
+            return 30
+        if granularity == "M1":
+            return 60
+        if granularity == "M2":
+            return 120
+        if granularity == "M4":
+            return 240
+        if granularity == "M5":
+            return 300
+        if granularity == "M10":
+            return 600
+        if granularity == "M15":
+            return 900
+        if granularity == "M30":
+            return 1800
+        if granularity == "H1":
+            return 3600
+        if granularity == "H2":
+            return 7200
+        if granularity == "H3":
+            return 10800
+        if granularity == "H4":
+            return 14400
+        if granularity == "H6":
+            return 21600
+        if granularity == "H8":
+            return 28800
+        if granularity == "H12":
+            return 43200
+        if granularity == "D":
+            return 86400
+        if granularity == "W":
+            return 604800
