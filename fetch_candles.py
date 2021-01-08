11098e1ff46c2f4f1509aaf9d6c8b6554088eda9
yukiinoue <yuki.inoue@desamis.co.jp>
2021-01-09 02:30:08 +0900

    NU3B を実装

--- fetch_candles.py
+++ fetch_candles.py
@@ -10,10 +10,10 @@ if __name__ == '__main__':
     oanda = Client(api_key, "", "practice")
     UTC = datetime.timezone.utc
     date_from = datetime.datetime(2020, 1, 1, 0, 0, tzinfo=UTC)
-    date_to = datetime.datetime(2021, 1, 1, 0, 0, tzinfo=UTC)
+    date_to = datetime.datetime(2020, 2, 1, 0, 0, tzinfo=UTC)
     start = time.time()
-    candles_df = oanda.get_candles("USD_JPY", date_from, date_to, "M1")
+    candles_df = oanda.get_candles("USD_JPY", date_from, date_to, "S5")
     elapsed_time = time.time() - start
     print(elapsed_time)
     candles_df.columns = ["Date Time", "Open", "High", "Low", "Close", "Volume"]
-    candles_df.to_csv("./data/candles/USD_JPY_M1_2020.csv", index=False)
+    candles_df.to_csv("./data/candles/USD_JPY_S5_2020.csv", index=False)
--- fetch_order_books.py
+++ fetch_order_books.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
     oanda = Client(api_key, "", "practice")
     UTC = datetime.timezone.utc
     date_from = datetime.datetime(2020, 1, 1, 0, 0, tzinfo=UTC)
-    date_to = datetime.datetime(2020, 1, 2, 0, 0, tzinfo=UTC)
+    date_to = datetime.datetime(2020, 2, 1, 0, 0, tzinfo=UTC)
     start = time.time()
     order_books = oanda.get_order_books("USD_JPY", date_from, date_to, True)
     order_books = {math.floor(o["unixTime"]): o["buckets"] for o in order_books}  # 辞書型に変換
